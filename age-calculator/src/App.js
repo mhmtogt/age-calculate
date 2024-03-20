6eb3263c40af9cf25af7a76ecbbb6062c2bcc7b3
Mehmet OGUT <mogut44949@gmail.com>
2024-03-20 12:02:40 +0300

    age calculator

---
+++ age-calculator/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import AgeCalculator from './Components/AgeCalculator';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AgeCalculator/>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
